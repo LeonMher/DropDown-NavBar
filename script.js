a353fc33061de723a5f8cd5f646909a92b20f3d3
Mher <99.p3rc3n7@gmail.com>
2022-01-11 13:55:36 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector('.navBarPhone');
+const button = document.querySelector('.btn');
+
+
+button.addEventListener('click', function(){
+
+    navbar.classList.toggle('newClass')
+
+})
